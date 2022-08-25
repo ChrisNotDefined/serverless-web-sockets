0989f1df1c6c24f90ba6610ce696b3eca78a5b29
Chris-CBQA <christopher@cbqasolutions.com>
2022-08-25 14:06:18 -0500

    refactor: onConnected and onDissconnected events

--- src/functions/ws/connection.ts
+++ src/functions/ws/connection.ts
@@ -1,47 +1,53 @@
 import {
   APIGatewayEventWebsocketRequestContextV2,
   APIGatewayProxyCallbackV2,
+  APIGatewayProxyResultV2,
   APIGatewayProxyWebsocketEventV2,
 } from 'aws-lambda';
-import { successfullReponse } from '@src/util/lambdaResponses';
+import * as log from 'lambda-log';
+import { buildErrorRespone, successfullReponse } from '@src/util/lambdaResponses';
 import { addConnection, deleteConnection } from '@src/connections/dynamoClient';
 import { sendWSConnected } from '@src/connections/wsSender';
 
-export const handler = (
+export const handler = async (
   event: APIGatewayProxyWebsocketEventV2,
   context: APIGatewayEventWebsocketRequestContextV2,
   callback: APIGatewayProxyCallbackV2
 ) => {
   const requestEventType = event.requestContext.eventType;
-  console.log(requestEventType);
+  log.debug('@@@ Event body', { event });
 
   if (requestEventType === 'CONNECT') {
     // Handle connection
-    addConnection(event.requestContext.connectionId)
-      .then(() => {
-        onConnected(event);
-        callback(null, successfullReponse);
-      })
-      .catch((err) => {
-        callback(null, JSON.stringify(err));
-      });
+    const response = await onConnected(event);
+    callback(null, response);
   } else if (requestEventType === 'DISCONNECT') {
     // Handle disconnection
-    deleteConnection(event.requestContext.connectionId)
-      .then(() => {
-        callback(null, successfullReponse);
-      })
-      .catch((err) => {
-        console.log(err);
-        callback(null, {
-          statusCode: 500,
-          body: 'Failed to connect: ' + JSON.stringify(err),
-        });
-      });
+    const response = await onDisconnected(event);
+    callback(null, response);
   }
 };
 
-const onConnected = (event: APIGatewayProxyWebsocketEventV2) => {
-  console.log('sendOnConnected');
-  sendWSConnected(event);
+const onConnected = async (event: APIGatewayProxyWebsocketEventV2): Promise<APIGatewayProxyResultV2> => {
+  try {
+    log.debug('@@@ OnConnected');
+    await addConnection(event.requestContext.connectionId);
+    await sendWSConnected(event);
+    return successfullReponse;
+  } catch (error) {
+    log.error(error as Error, { msg: '@ Connect error' });
+    return buildErrorRespone(error as Error, 'Failed to connect to server');
+  }
+};
+
+const onDisconnected = async (event: APIGatewayProxyWebsocketEventV2): Promise<APIGatewayProxyResultV2> => {
+  try {
+    log.debug('@@@ OnDissconnected');
+    const { connectionId } = event.requestContext;
+    await deleteConnection(connectionId);
+    return successfullReponse;
+  } catch (error) {
+    log.error('@ Disconnect error');
+    return buildErrorRespone(error as Error, 'Error when failing to disconnect to server');
+  }
 };
--- src/util/lambdaResponses.ts
+++ src/util/lambdaResponses.ts
@@ -1,6 +1,23 @@
 import { APIGatewayProxyResultV2 } from 'aws-lambda';
 
+interface ErrorBody {
+  message: string;
+  error: string;
+}
+
 export const successfullReponse: APIGatewayProxyResultV2 = {
   statusCode: 200,
   body: 'Request completed sucessfully',
+};
+
+export const buildErrorRespone = (error: Error, msg: string): APIGatewayProxyResultV2 => {
+  const errorBody: ErrorBody = {
+    error: error.message,
+    message: msg,
+  };
+
+  return {
+    statusCode: 500,
+    body: JSON.stringify(errorBody),
+  };
 };
